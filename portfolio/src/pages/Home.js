97b8e6e34ae20f3f6c30bb36716d01dd0449505a
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-04-05 11:15:05 +0530

    add picture

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -3,41 +3,37 @@ import '../style/Home.css';
 export default function Home() {
   return (
     <div className="home-page">
-      
-  <nav class="navbar">
-    <div class="nav-left">Home</div>
-    <div class="nav-right">
-      <a href="#">About Me</a>
-      <a href="#">Education</a>
-      <a href="#">Skills</a>
-      <a href="#">Projects</a>
-      <a href="#">Contact</a>
-    </div>
-  </nav>
+      <nav className="navbar">
+        <div className="nav-left">Home</div>
+        <div className="nav-right">
+          <a href="#">About Me</a>
+          <a href="#">Education</a>
+          <a href="#">Skills</a>
+          <a href="#">Projects</a>
+          <a href="#">Contact</a>
+        </div>
+      </nav>
 
-  
-  <section class="hero">
+      <section className="hero">
+        <h1 className="title">HI, I'M RAMESH</h1>
 
-    <h1 class="title">HI, I'M RAMESH</h1>
+        <div className="left-text">
+          <h2 className="ai">AI</h2>
+          <h2 className="and">&</h2>
+          <h2 className="ml">ML</h2>
+          <p className="ent">Enthusiast</p>
+        </div>
 
-   
-    <div class="left-text">
-      <h2>AI & ML</h2>
-      <p>Enthusiast</p>
-    </div>
-
-   
-    <div class="right-text">
-      <p class="small">Passionate</p>
-      <h2>Full-Stack</h2>
-      <p class="small">Developer</p>
-    </div>
-
-    
-    <img src="mine.png" alt="Ramesh" class="profile"/>
-
-  </section>
-
+        <div className="right-text">
+          <p className="small">Passionate</p>
+          <h2>Full-Stack</h2>
+          <p className="small">Developer</p>
+        </div>
+        <div className="img">
+            <img src="/assests/mine.png" alt="Ramesh" className="profile" />
+        </div>
+        
+      </section>
     </div>
   );
 }
